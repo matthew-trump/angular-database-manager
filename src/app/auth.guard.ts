84e8682b6a060ad91b9851dc1c97a4a09eaec6ed
matthew-trump <matthew.trump@gmail.com>
2019-03-29 09:31:24 -0700

    protector for admin routes

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router){
+
+  }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      //if(environment.production){
+        const isLoggedIn = this.authService.isLoggedIn();
+        if(!isLoggedIn){
+            this.router.navigate(['login']); 
+        }
+        return isLoggedIn;
+  }
+}
