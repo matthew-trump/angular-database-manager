7266fd498dad05a5e96e4ab7f40bb152cbcf6436
matthew-trump <matthew.trump@gmail.com>
2019-04-05 20:47:28 -0700

    removing dependence on target explicitly

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -2,12 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpEvent, HttpHandler, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
-import { AuthService } from './auth.service';
+import { BackendApiService } from './backend-api.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-    constructor(public authService: AuthService) {
+    constructor(public backendApiService: BackendApiService) {
 
     }
     intercept(req: HttpRequest<any>,
@@ -17,8 +17,8 @@ export class AuthInterceptor implements HttpInterceptor {
         const l = document.createElement("a");
         l.href = req.url;
 
-        const target: any = environment.targets[this.authService.getCurrentTarget()];
-        if (l.pathname.startsWith(target.apiPath)) {
+        const targetConfig: any = environment.targets[this.backendApiService.target];
+        if (l.pathname.startsWith(targetConfig.apiPath)) {
             const idToken = localStorage.getItem("id_token");
             if (idToken) {
                 const cloned = req.clone({
