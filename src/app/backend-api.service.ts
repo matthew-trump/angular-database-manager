094767864488556a4c1fdb8d2213360fd49fc0ac
matthew-trump <matthew.trump@gmail.com>
2019-04-05 20:48:06 -0700

    putting target variable here

--- src/app/backend-api.service.ts
+++ src/app/backend-api.service.ts
@@ -19,6 +19,7 @@ import { ScheduleItemDeleteResponse } from './schedule-item-delete-response';
 import { ScheduleQuery } from './schedule-query';
 
 import { TestApiResponse } from './test-api-response';
+const DEFAULT_TARGET: any = environment.defaultTarget;
 const TARGETS: any = environment.targets;
 const TARGET_PARAM: string = "__target__";
 
@@ -26,67 +27,76 @@ const TARGET_PARAM: string = "__target__";
     providedIn: 'root'
 })
 export class BackendApiService {
+    public target: string = localStorage.getItem('target') || DEFAULT_TARGET;
     constructor(public http: HttpClient) { }
-    getBaseApiPath(target: string): string {
+    login(target: string, username: string, password: string): Observable<any> {
         const url: string = TARGETS[target].url;
         const apiPath: string = TARGETS[target].apiPath;
+        return this.http.post<any>(url + apiPath + 'login', { username, password });
+    }
+    getSchema(target: string): Observable<SchemaResponse> {
+        const url: string = TARGETS[target].url;
+        const apiPath: string = TARGETS[target].apiPath;
+        return this.http.get<SchemaResponse>(url + apiPath + 'config/schema');
+    }
+    setTarget(target: string) {
+        this.target = target;
+    }
+    getBaseApiPath(): string {
+        const url: string = TARGETS[this.target].url;
+        const apiPath: string = TARGETS[this.target].apiPath;
         return url + apiPath;
     }
-    getQueryString(target: string, query: EntitiesQuery | ScheduleQuery): string {
-        const full = Object.assign({}, query, { [TARGET_PARAM]: target });
+    getQueryString(query: EntitiesQuery | ScheduleQuery): string {
+        const full = Object.assign({}, query, { [TARGET_PARAM]: this.target });
         const queryString = Object.keys(full).map(key => key + '=' + full[key]).join('&');
         return queryString;
     }
-    login(target: string, username: string, password: string): Observable<any> {
-        return this.http.post<any>(this.getBaseApiPath(target) + 'login', { username, password });
-    }
-    getSchema(target: string): Observable<SchemaResponse> {
-        return this.http.get<SchemaResponse>(this.getBaseApiPath(target) + 'config/schema');
-    }
 
-    getEntities(target: string, plural: string, query?: EntitiesQuery): Observable<EntitiesResponse> {
-        const queryString: string = "?" + this.getQueryString(target, query);
-        return this.http.get<EntitiesResponse>(this.getBaseApiPath(target) + 'entities/' + plural + queryString);
+    getEntities(plural: string, query?: EntitiesQuery): Observable<EntitiesResponse> {
+        const queryString: string = "?" + this.getQueryString(query);
+        return this.http.get<EntitiesResponse>(this.getBaseApiPath() + 'entities/' + plural + queryString);
     }
-    updateEntity(target: string, plural: string, id: number, update: EntityUpdate): Observable<EntityUpdateResponse> {
-        return this.http.put<EntityUpdateResponse>(this.getBaseApiPath(target) + 'entities/' + plural + "/" + id, { update: update });
+    updateEntity(plural: string, id: number, update: EntityUpdate): Observable<EntityUpdateResponse> {
+        return this.http.put<EntityUpdateResponse>(this.getBaseApiPath() + 'entities/' + plural + "/" + id, { update: update });
     }
-    addEntities(target: string, plural: string, entities: Entity[]): Observable<EntityInsertResponse> {
-        return this.http.post<EntityInsertResponse>(this.getBaseApiPath(target) + 'entities/' + plural, { entities: entities });
+    addEntities(plural: string, entities: Entity[]): Observable<EntityInsertResponse> {
+        return this.http.post<EntityInsertResponse>(this.getBaseApiPath() + 'entities/' + plural, { entities: entities });
     }
-    getSchedule(target: string, query?: ScheduleQuery): Observable<any> {
-        const queryString: string = "?" + this.getQueryString(target, query);
-        return this.http.get<any>(this.getBaseApiPath(target) + 'schedule' + queryString);
+    getSchedule(query?: ScheduleQuery): Observable<any> {
+        const queryString: string = "?" + this.getQueryString(query);
+        return this.http.get<any>(this.getBaseApiPath() + 'schedule' + queryString);
     }
-    updateScheduleItem(target: string, id: number, update: ScheduleItemUpdate): Observable<ScheduleItemUpdateResponse> {
-        return this.http.put<ScheduleItemUpdateResponse>(this.getBaseApiPath(target) + 'schedule/' + id, { update: update });
+    updateScheduleItem(id: number, update: ScheduleItemUpdate): Observable<ScheduleItemUpdateResponse> {
+        return this.http.put<ScheduleItemUpdateResponse>(this.getBaseApiPath() + 'schedule/' + id, { update: update });
     }
-    addScheduleItems(target: string, items: ScheduleItemUpdate[]): Observable<ScheduleItemUpdateResponse> {
-        return this.http.post<ScheduleItemUpdateResponse>(this.getBaseApiPath(target) + 'schedule', { items: items });
+    addScheduleItems(items: ScheduleItemUpdate[]): Observable<ScheduleItemUpdateResponse> {
+        return this.http.post<ScheduleItemUpdateResponse>(this.getBaseApiPath() + 'schedule', { items: items });
     }
-    deleteScheduleItem(target: string, id: number): Observable<ScheduleItemDeleteResponse> {
-        return this.http.delete<ScheduleItemDeleteResponse>(this.getBaseApiPath(target) + 'schedule/' + id);
+    deleteScheduleItem(id: number): Observable<ScheduleItemDeleteResponse> {
+        return this.http.delete<ScheduleItemDeleteResponse>(this.getBaseApiPath() + 'schedule/' + id);
     }
-    getCurrentScheduleItem(target: string): Observable<any> {
-        return this.http.get<any>(this.getBaseApiPath(target) + 'current');
+    getCurrentScheduleItem(): Observable<any> {
+        return this.http.get<any>(this.getBaseApiPath() + 'current');
     }
-    getCurrentScheduleInstance(target: string): Observable<any> {
-        return this.http.get<any>(this.getBaseApiPath(target) + 'round');
+    getCurrentScheduleInstance(): Observable<any> {
+        return this.http.get<any>(this.getBaseApiPath() + 'instance');
     }
 
 
-    ping(target: string): Observable<TestApiResponse> {
-        return this.http.get<TestApiResponse>(this.getBaseApiPath(target) + 'ping');
+    ping(): Observable<TestApiResponse> {
+        return this.http.get<TestApiResponse>(this.getBaseApiPath() + 'ping');
     }
-    databasePing(target: string, value: string): Observable<TestApiResponse> {
-        return this.http.post<TestApiResponse>(this.getBaseApiPath(target) + 'database/ping',
+    protected(): Observable<TestApiResponse> {
+        return this.http.get<TestApiResponse>(this.getBaseApiPath() + 'protected');
+    }
+    databasePing(value: string): Observable<TestApiResponse> {
+        return this.http.post<TestApiResponse>(this.getBaseApiPath() + 'database/ping',
             { key: value });
     }
-    databasePings(target: string): Observable<TestApiResponse> {
-        return this.http.get<TestApiResponse>(this.getBaseApiPath(target) + 'database/ping');
+    databasePings(): Observable<TestApiResponse> {
+        return this.http.get<TestApiResponse>(this.getBaseApiPath() + 'database/ping');
     }
-    protected(target: string): Observable<TestApiResponse> {
-        return this.http.get<TestApiResponse>(this.getBaseApiPath(target) + 'protected');
-    }
+
 
 }
