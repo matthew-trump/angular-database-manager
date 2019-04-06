cce67081243e8fb9557a1f270d80a3506c5beee0
matthew-trump <matthew.trump@gmail.com>
2019-04-05 20:47:04 -0700

    changing initialization after target observable refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { ConfigSchemaService } from './config-schema.service';
+import { BackendApiService } from './backend-api.service';
 import { environment } from 'src/environments/environment';
+import { Schema } from './schema';
+import { EntityConfig } from './entity-config';
+import { Store } from '@ngrx/store';
+
 
 const DEFAULT_TITLE: string = 'Actions on Google Database Manager';
 const TARGETS: any = environment.targets;
@@ -12,22 +17,36 @@ const TARGETS: any = environment.targets;
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   public TARGETS = TARGETS;
   public title: string = DEFAULT_TITLE;
+  public entities: string[];
+
   constructor(
     public authService: AuthService,
     public configSchemaService: ConfigSchemaService,
+    public backendApiService: BackendApiService,
+    private store: Store<any>,
     private router: Router) {
-    const target: string = this.authService.currentTarget;
-    console.log(TARGETS);
-    this.configSchemaService.loadSchema(target).then(loaded => {
-      console.log(loaded);
-      if (!loaded) {
+
+    this.store.select('config').subscribe((state: any) => {
+      if (state.schema) {
+        this.entities = state.schema.entities.map((entityConfig: EntityConfig) => {
+          return entityConfig.plural;
+        });
+      } else if (state.target) {
         console.log("LOGGING OUT");
         this.logout();
+        this.entities = null;
       }
-    });
+    })
+    try {
+      this.configSchemaService.loadSchema(this.backendApiService.target);
+    } catch (err) { }
+
   }
+
+
   logout() {
     this.authService.logout();
     this.router.navigate(['login']);
